751bea24d54cb0ae3bbe3b0945022106257284e6
Ethan Berry <ethanberry670@gmail.com>
2026-05-05 10:39:27 +0100

    Feat: Add seed data

--- RecordShop/RecordShop.Api/Data/RecordShopContext.cs
+++ RecordShop/RecordShop.Api/Data/RecordShopContext.cs
@@ -7,5 +7,15 @@ namespace RecordShop.Api.Data
     {
         public DbSet<Album> Albums { get; set; }
         public RecordShopContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Album>().HasData(
+                new Album { Title = "Thriller", Artist = "Michael Jackson", Price = 10.99M },
+                new Album { Title = "Back In Black", Artist = "AC/DC", Price = 9.49M },
+                new Album { Title = "The Dark Side of the Moon", Artist = "Pink Floyd", Price = 15.00M },
+                new Album { Title = "Nevermind", Artist = "Nirvana", Price = 5.99M }
+            );
+        }
     }
 }
