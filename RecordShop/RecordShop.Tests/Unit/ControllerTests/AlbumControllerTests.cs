414a1453862ab771c98b22763a6158af598a797d
Ethan Berry <ethanberry670@gmail.com>
2026-05-05 13:53:40 +0100

    Feat: New test for happy path

--- RecordShop/RecordShop.Tests/Unit/ControllerTests/AlbumControllerTests.cs
+++ RecordShop/RecordShop.Tests/Unit/ControllerTests/AlbumControllerTests.cs
@@ -40,5 +40,28 @@ namespace RecordShop.Tests.Unit.ControllerTests
             value.Should().NotBeNull();
             value.Should().BeEmpty();
         }
+
+        [Test]
+        public async Task AlbumController_ShouldReturnOkWithList_WhenServiceReturnsSeededList()
+        {
+            var testList = new List<Album>
+            {
+                new Album {Title = "Test Title1", Artist = "Test Artist1", Price = 0.00M },
+                new Album {Title = "Test Title2", Artist = "Test Artist2", Price = 0.00M },
+                new Album {Title = "Test Title3", Artist = "Test Artist3", Price = 0.00M },
+                new Album {Title = "Test Titl4", Artist = "Test Artist4", Price = 0.00M },
+                new Album {Title = "Test Title5", Artist = "Test Artist5", Price = 0.00M },
+            };
+
+            _albumServiceMock.Setup(a => a.GetAllAlbums()).ReturnsAsync(testList);
+
+            var result = await _albumController.GetAllAlbums();
+
+            var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+            var value = okResult.Value as List<Album>;
+
+            value.Should().NotBeNull();
+            value.Should().BeEquivalentTo(testList);
+        }
     }
 }
