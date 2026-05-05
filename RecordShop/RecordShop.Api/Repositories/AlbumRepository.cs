0047a33f73f0883515917de05ec1a115cd1b02e5
Ethan Berry <ethanberry670@gmail.com>
2026-05-05 10:08:15 +0100

    Feat: Add repository layer

---
+++ RecordShop/RecordShop.Api/Repositories/AlbumRepository.cs
@@ -0,0 +1,12 @@
+﻿using RecordShop.Api.Models.DataModels;
+
+namespace RecordShop.Api.Repositories
+{
+    public class AlbumRepository
+    {
+        //public async Task<Album?> GetAllAlbums()
+        //{
+            
+        //}
+    }
+}
