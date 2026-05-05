0a752251f9521b9f7a49dfea18dcd16f20ddec88
Ethan Berry <ethanberry670@gmail.com>
2026-05-05 13:58:45 +0100

    Feat: Added exception middleware class

---
+++ RecordShop/RecordShop.Api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace RecordShop.Api.Middleware
+{
+    public class ExceptionMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
