44b8993921cf99920fd5e02c71eba8aa01249688
Ethan Berry <ethanberry670@gmail.com>
2026-05-05 11:17:03 +0100

    Feat: Add happy path test

--- RecordShop/RecordShop.Tests/Unit/ServiceTests/AlbumServiceTests.cs
+++ RecordShop/RecordShop.Tests/Unit/ServiceTests/AlbumServiceTests.cs
@@ -27,5 +27,25 @@ namespace RecordShop.Tests.Unit.ServiceTests
 
             result.Should().BeEqualTo(null);
         }
+
+        [Test]
+        public async Task GetAllAlbums_ShouldReturnList_WhenRepositoryReturnsList()
+        {
+            var testAlbumList = new List<Album>
+            {
+                new Album {Title = "Test Title1", Artist = "Test Artist1", Price = 0.00M },
+                new Album {Title = "Test Title2", Artist = "Test Artist2", Price = 0.00M },
+                new Album {Title = "Test Title3", Artist = "Test Artist3", Price = 0.00M },
+                new Album {Title = "Test Titl4", Artist = "Test Artist4", Price = 0.00M },
+                new Album {Title = "Test Title5", Artist = "Test Artist5", Price = 0.00M },
+            };
+
+            _albumRepositoryMock.Setup(a => a.GetAllAlbums()).ReturnsAsync(testAlbumList);
+
+            var result = await _albumService.GetAllAlbums();
+
+            result.Should().BeEqualTo(testAlbumList);
+            result.Should().HaveCount(5);
+        }
     }
 }
