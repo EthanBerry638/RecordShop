1c296ee4147d442500bb360b1e6cdd81ac2b5f0e
Ethan Berry <ethanberry670@gmail.com>
2026-05-05 16:08:19 +0100

    Feat: Middleware now specifically catches argumentexception

--- RecordShop/RecordShop.Api/Middleware/ExceptionMiddleware.cs
+++ RecordShop/RecordShop.Api/Middleware/ExceptionMiddleware.cs
@@ -8,16 +8,26 @@
             {
                 await next(context);
             }
+            catch (ArgumentException ex)
+            {
+                await HandleArgumentException(ex, context);
+            }
             catch (Exception ex)
             {
-                await HandleException(ex, context);
+                await HandleGeneralException(ex, context);
             }
         }
 
-        private async Task HandleException(Exception ex, HttpContext context)
+        private async Task HandleGeneralException(Exception ex, HttpContext context)
         {
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
             await context.Response.WriteAsJsonAsync(new { message = "Server is down :(" });
+        }
+
+        private async Task HandleArgumentException(Exception ex, HttpContext context)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsJsonAsync(new { message = "Please enter a number greater than 0" });
         }
     }
 }
