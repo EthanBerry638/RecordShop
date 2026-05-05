4480f881d1345b01d43a64afb357b1be9471d6d4
Ethan Berry <ethanberry670@gmail.com>
2026-05-05 10:23:32 +0100

    Feat: Add interface for repository

--- RecordShop/RecordShop.Api/Repositories/AlbumRepository.cs
+++ RecordShop/RecordShop.Api/Repositories/AlbumRepository.cs
@@ -2,7 +2,7 @@
 
 namespace RecordShop.Api.Repositories
 {
-    public class AlbumRepository
+    public class AlbumRepository : IAlbumRepository
     {
         public async Task<List<Album?>> GetAllAlbums()
         {
---
+++ RecordShop/RecordShop.Api/Repositories/IAlbumRepository.cs
@@ -0,0 +1,9 @@
+﻿using RecordShop.Api.Models.DataModels;
+
+namespace RecordShop.Api.Repositories
+{
+    public interface IAlbumRepository
+    {
+        Task<List<Album?>> GetAllAlbums();
+    }
+}
