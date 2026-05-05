c71007d996e9a0fab4dd21193ff2f4ed34d545b5
Ethan Berry <ethanberry670@gmail.com>
2026-05-05 12:27:17 +0100

    Feat: Setup albumcontrollertests

---
+++ RecordShop/RecordShop.Tests/Unit/ControllerTests/AlbumControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using RecordShop.Api.Controllers;
+using RecordShop.Api.Services;
+
+namespace RecordShop.Tests.Unit.ControllerTests
+{
+    public class AlbumControllerTests
+    {
+        private Mock<IAlbumService> _albumServiceMock;
+        private AlbumController _albumController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _albumServiceMock = new Mock<IAlbumService>();
+            _albumController = new AlbumController(_albumServiceMock.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _albumController.Dispose();
+        }
+    }
+}
