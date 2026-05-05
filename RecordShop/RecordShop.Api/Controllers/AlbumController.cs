df4acdd78f146579f706c89597e93a1dc5052e28
Ethan Berry <ethanberry670@gmail.com>
2026-05-05 10:22:38 +0100

    Feat: Add controller

---
+++ RecordShop/RecordShop.Api/Controllers/AlbumController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using RecordShop.Api.Services;
+
+namespace RecordShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        private readonly IAlbumService _albumService;
+
+        public AlbumController(IAlbumService albumSerivce)
+        {
+            _albumService = albumSerivce;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAlbums()
+        {
+            var albums = await _albumService.GetAllAlbums();
+            return Ok(albums);
+        }
+    }
+}
