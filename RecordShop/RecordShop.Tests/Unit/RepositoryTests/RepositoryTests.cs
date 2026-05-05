e49263364d3a596bf36596bde32b89ecca4def29
Ethan Berry <ethanberry670@gmail.com>
2026-05-05 12:00:26 +0100

    Feat: Add setup for repository tests

--- RecordShop/RecordShop.Tests/Unit/RepositoryTests/RepositoryTests.cs
+++ RecordShop/RecordShop.Tests/Unit/RepositoryTests/RepositoryTests.cs
@@ -1,6 +1,38 @@
-﻿namespace RecordShop.Tests.Unit.RepositoryTests
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using RecordShop.Api.Data;
+using RecordShop.Api.Repositories;
+
+namespace RecordShop.Tests.Unit.RepositoryTests
 {
     public class RepositoryTests
     {
+        private AlbumRepository _albumRepository;
+        private RecordShopContext _context;
+        private SqliteConnection _connection;
+
+        [SetUp]
+        public void Setup()
+        {
+            _connection = new SqliteConnection("Filename=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<RecordShopContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            _context = new RecordShopContext(options);
+
+            _context.Database.EnsureCreated();
+
+            _albumRepository = new AlbumRepository(_context);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+            _connection.Dispose();  
+        }
     }
 }
