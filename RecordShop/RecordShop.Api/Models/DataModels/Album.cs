bbd5382f65149cf3aa9cca9656d04c688b5443c7
Ethan Berry <ethanberry670@gmail.com>
2026-05-06 12:09:32 +0100

    Update: Album now has 255 char constraints

--- RecordShop/RecordShop.Api/Models/DataModels/Album.cs
+++ RecordShop/RecordShop.Api/Models/DataModels/Album.cs
@@ -1,9 +1,13 @@
-﻿namespace RecordShop.Api.Models.DataModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecordShop.Api.Models.DataModels
 {
     public class Album
     {
         public int Id { get; init; }
+        [MaxLength(255)]
         public required string Title { get; set; }
+        [MaxLength(255)]
         public required string Artist { get; set; }
         public decimal Price { get; set; }
     }
