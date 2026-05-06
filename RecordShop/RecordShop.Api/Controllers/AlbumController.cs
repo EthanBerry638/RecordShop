1ec8096e73b2a2ea4537dd3bfc91e0a1c8ba16a3
Ethan Berry <ethanberry670@gmail.com>
2026-05-06 12:32:43 +0100

    Feat: Add placeholder method for testing

--- RecordShop/RecordShop.Api/Controllers/AlbumController.cs
+++ RecordShop/RecordShop.Api/Controllers/AlbumController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RecordShop.Api.Models.DTOs;
 using RecordShop.Api.Services;
 
 namespace RecordShop.Api.Controllers
@@ -33,6 +34,12 @@ namespace RecordShop.Api.Controllers
             }
 
             return Ok(album);
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> PostAlbumAsync(PostAlbumRequestResponse requestDTO)
+        {
+            return Ok();
         }
     }
 }
