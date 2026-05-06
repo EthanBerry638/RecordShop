bf710edccac4205c093d6f21b18de8f0f6b746e9
EthanBerry638 <ethanberry670@gmail.com>
2026-05-06 19:05:15 +0100

    Refactor: Cleaned up exception handling middleware

--- RecordShop/RecordShop.Api/Middleware/ExceptionMiddleware.cs
+++ RecordShop/RecordShop.Api/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,4 @@
-﻿using RecordShop.Api.CustomExceptions;
-
-namespace RecordShop.Api.Middleware
+﻿namespace RecordShop.Api.Middleware
 {
     public class ExceptionMiddleware : IMiddleware
     {
@@ -9,22 +7,6 @@ namespace RecordShop.Api.Middleware
             try
             {
                 await next(context);
-            }
-            catch (LongStringException ex)
-            {
-                await HandleLongStringException(ex, context);
-            }
-            catch (EmptyStringException ex)
-            {
-                await HandleEmptyStringException(ex, context);
-            }
-            catch (InvalidPriceException ex)
-            {
-                await HandleInvalidPriceException(ex, context);
-            }
-            catch (ArgumentException ex)
-            {
-                await HandleArgumentException(ex, context);
             }
             catch (Exception ex)
             {
@@ -36,29 +18,6 @@ namespace RecordShop.Api.Middleware
         {
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
             await context.Response.WriteAsJsonAsync(new { message = "Server is down :(" });
-        }
-
-        private async Task HandleArgumentException(Exception ex, HttpContext context)
-        {
-            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            await context.Response.WriteAsJsonAsync(new { message = "Please enter a number greater than 0" });
-        }
-
-        private async Task HandleInvalidPriceException(Exception ex, HttpContext context)
-        {
-            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            await context.Response.WriteAsJsonAsync(new { message = "Please enter a price greater than or equal to 0 and lower than 2 million" });
-        }
-
-        private async Task HandleEmptyStringException(Exception ex, HttpContext context)
-        {
-            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            await context.Response.WriteAsJsonAsync(new { message = "Please enter something" });
-        }
-        private async Task HandleLongStringException(Exception ex, HttpContext context)
-        {
-            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            await context.Response.WriteAsJsonAsync(new { message = "Please don't enter more than 255 chars" });
         }
     }
 }
