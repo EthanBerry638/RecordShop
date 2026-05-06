b0070380a7075c08eb04fd0c0a25c6efe67aafff
Ethan Berry <ethanberry670@gmail.com>
2026-05-06 14:32:48 +0100

    Feat: Add Put DTOS

---
+++ RecordShop/RecordShop.Api/Models/DTOs/PutAlbumRequestResponse.cs
@@ -0,0 +1,5 @@
+﻿namespace RecordShop.Api.Models.DTOs
+{
+    public record PutAlbumRequest(string Title, string Artist, decimal Price) {}
+    public record PutAlbumResponse(int Id, string Title, string Artist, decimal Price) {}
+}
