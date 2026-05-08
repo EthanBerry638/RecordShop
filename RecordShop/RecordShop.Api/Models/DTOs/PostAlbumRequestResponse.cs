86f7f9a92addc41c24c499374646bfc7d3d969f5
Ethan Berry <ethanberry670@gmail.com>
2026-05-08 22:52:02 +0100

    Removed data annotations from response DTOs

--- RecordShop/RecordShop.Api/Models/DTOs/PostAlbumRequestResponse.cs
+++ RecordShop/RecordShop.Api/Models/DTOs/PostAlbumRequestResponse.cs
@@ -9,9 +9,9 @@ namespace RecordShop.Api.Models.DTOs
     );
 
     public record PostAlbumResponse(
-        [Required] int Id,
-        [Required][MaxLength(150, ErrorMessage = "Title cannot exceed 150 characters")] string Title,
-        [Required][MaxLength(150, ErrorMessage = "Artist cannot exceed 150 characters")] string Artist,
-        [Range(0.01, 2000000, ErrorMessage = "Price must be between 0.01 and 2,000,000")] decimal Price
+        int Id,
+        string Title,
+        string Artist,
+        decimal Price
     );
 }
