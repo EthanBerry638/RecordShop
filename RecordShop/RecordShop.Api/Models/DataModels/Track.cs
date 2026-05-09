fb1787177c1156c99aaec53eaecff8d0fcf8baa6
Ethan Berry <ethanberry670@gmail.com>
2026-05-09 11:30:28 +0100

    Feat: Add track data model

---
+++ RecordShop/RecordShop.Api/Models/DataModels/Track.cs
@@ -0,0 +1,11 @@
+﻿namespace RecordShop.Api.Models.DataModels
+{
+    public class Track
+    {
+        public int Id { get; set; }
+        public int DurationInSec { get; set; }
+        public int AblumId { get; set; }
+        public Album Album { get; set; }
+        public List<Genre> Genres { get; set; } = new();
+    }
+}
