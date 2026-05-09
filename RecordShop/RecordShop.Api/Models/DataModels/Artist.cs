27bf7a0a4ec21e9cccbba4a538568713ffc5ebd5
Ethan Berry <ethanberry670@gmail.com>
2026-05-09 11:32:30 +0100

    Feat: Add artist data model

---
+++ RecordShop/RecordShop.Api/Models/DataModels/Artist.cs
@@ -0,0 +1,12 @@
+﻿namespace RecordShop.Api.Models.DataModels
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Bio { get; set; }
+        public int Age { get; set; }
+        public List<Genre> Genres { get; set; } = new();
+        public List<Album> Albums { get; set; } = new();
+    }
+}
