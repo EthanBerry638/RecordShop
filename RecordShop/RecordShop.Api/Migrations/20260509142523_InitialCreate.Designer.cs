9295d0be9ae85a1e3e42041c8760d012c1cd40b2
Ethan Berry <ethanberry670@gmail.com>
2026-05-09 15:26:30 +0100

    Feat: Add new migration with updated schema for production

--- RecordShop/RecordShop.Api/Migrations/20260509142523_InitialCreate.Designer.cs
+++ RecordShop/RecordShop.Api/Migrations/20260509142523_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecordShop.Api.Data;
 
@@ -10,9 +11,11 @@ using RecordShop.Api.Data;
 namespace RecordShop.Api.Migrations
 {
     [DbContext(typeof(RecordShopContext))]
-    partial class RecordShopContextModelSnapshot : ModelSnapshot
+    [Migration("20260509142523_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.26");
---
+++ RecordShop/RecordShop.Api/Migrations/20260509142523_InitialCreate.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordShop.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 250, nullable: false),
+                    ReleaseDate = table.Column<DateOnly>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", precision: 9, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Artist",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Bio = table.Column<string>(type: "TEXT", nullable: true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artist", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Track",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DurationInSec = table.Column<int>(type: "INTEGER", nullable: false),
+                    AlbumId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Track", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Track_Albums_AlbumId",
+                        column: x => x.AlbumId,
+                        principalTable: "Albums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AlbumArtist",
+                columns: table => new
+                {
+                    AlbumsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ArtistsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlbumArtist", x => new { x.AlbumsId, x.ArtistsId });
+                    table.ForeignKey(
+                        name: "FK_AlbumArtist_Albums_AlbumsId",
+                        column: x => x.AlbumsId,
+                        principalTable: "Albums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AlbumArtist_Artist_ArtistsId",
+                        column: x => x.ArtistsId,
+                        principalTable: "Artist",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AlbumGenre",
+                columns: table => new
+                {
+                    AlbumsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    GenresId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlbumGenre", x => new { x.AlbumsId, x.GenresId });
+                    table.ForeignKey(
+                        name: "FK_AlbumGenre_Albums_AlbumsId",
+                        column: x => x.AlbumsId,
+                        principalTable: "Albums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AlbumGenre_Genre_GenresId",
+                        column: x => x.GenresId,
+                        principalTable: "Genre",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArtistGenre",
+                columns: table => new
+                {
+                    ArtistsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    GenresId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArtistGenre", x => new { x.ArtistsId, x.GenresId });
+                    table.ForeignKey(
+                        name: "FK_ArtistGenre_Artist_ArtistsId",
+                        column: x => x.ArtistsId,
+                        principalTable: "Artist",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArtistGenre_Genre_GenresId",
+                        column: x => x.GenresId,
+                        principalTable: "Genre",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GenreTrack",
+                columns: table => new
+                {
+                    GenresId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TracksId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GenreTrack", x => new { x.GenresId, x.TracksId });
+                    table.ForeignKey(
+                        name: "FK_GenreTrack_Genre_GenresId",
+                        column: x => x.GenresId,
+                        principalTable: "Genre",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GenreTrack_Track_TracksId",
+                        column: x => x.TracksId,
+                        principalTable: "Track",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlbumArtist_ArtistsId",
+                table: "AlbumArtist",
+                column: "ArtistsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlbumGenre_GenresId",
+                table: "AlbumGenre",
+                column: "GenresId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArtistGenre_GenresId",
+                table: "ArtistGenre",
+                column: "GenresId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GenreTrack_TracksId",
+                table: "GenreTrack",
+                column: "TracksId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Track_AlbumId",
+                table: "Track",
+                column: "AlbumId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlbumArtist");
+
+            migrationBuilder.DropTable(
+                name: "AlbumGenre");
+
+            migrationBuilder.DropTable(
+                name: "ArtistGenre");
+
+            migrationBuilder.DropTable(
+                name: "GenreTrack");
+
+            migrationBuilder.DropTable(
+                name: "Artist");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+
+            migrationBuilder.DropTable(
+                name: "Track");
+
+            migrationBuilder.DropTable(
+                name: "Albums");
+        }
+    }
+}
