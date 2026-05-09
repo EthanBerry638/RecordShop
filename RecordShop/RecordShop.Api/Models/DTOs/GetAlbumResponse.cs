bf0ba54254fffee042b76c98aff1972c672baca5
Ethan Berry <ethanberry670@gmail.com>
2026-05-09 14:33:29 +0100

    Feat: Add get album response dto

---
+++ RecordShop/RecordShop.Api/Models/DTOs/GetAlbumResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace RecordShop.Api.Models.DTOs
+{
+    public record GetAlbumResponse
+    (
+        int Id,
+        string Title,
+        string Description,
+        DateOnly? ReleaseDate,
+        decimal Price
+    )
+    {}
+}
