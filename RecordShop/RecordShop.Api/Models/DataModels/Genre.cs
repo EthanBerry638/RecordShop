7b4420f16184a1b7dd4860c5b69ff4df3c47d306
Ethan Berry <ethanberry670@gmail.com>
2026-05-09 11:33:52 +0100

    Feat: Add genre data model

---
+++ RecordShop/RecordShop.Api/Models/DataModels/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace RecordShop.Api.Models.DataModels
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<Album> Albums { get; set; } = new();
+        public List<Track> Tracks { get; set; } = new();
+        public List<Artist> Artists { get; set; } = new();
+    }
+}
