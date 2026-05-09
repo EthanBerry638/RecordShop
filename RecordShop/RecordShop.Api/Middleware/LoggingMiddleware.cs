cf5c2bf4bfa005da214d9507edc63aa45c68a1ca
Ethan Berry <ethanberry670@gmail.com>
2026-05-09 22:45:21 +0100

    Feat: Add logging middleware class and setup layout

---
+++ RecordShop/RecordShop.Api/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace RecordShop.Api.Middleware
+{
+    public class LoggingMiddleware : IMiddleware
+    {
+        private readonly ILogger<LoggingMiddleware> _logger;
+
+        public LoggingMiddleware(ILogger<LoggingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            
+            await next(context);
+        }
+    }
+}
