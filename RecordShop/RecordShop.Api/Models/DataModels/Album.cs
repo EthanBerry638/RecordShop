a5297efce5f03a66377d72df91ddbcb090957bff
Ethan Berry <ethanberry670@gmail.com>
2026-05-09 11:35:55 +0100

    Update: Album data model now relates to new models

--- RecordShop/RecordShop.Api/Models/DataModels/Album.cs
+++ RecordShop/RecordShop.Api/Models/DataModels/Album.cs
@@ -6,7 +6,11 @@ namespace RecordShop.Api.Models.DataModels
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
-        public string Artist { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime? ReleaseDate { get; set; }
         public decimal Price { get; set; }
+        public List<Track> Tracks { get; set; } = new();
+        public List<Artist> Artists { get; set; } = new();
+        public List<Genre> Genres { get; set; } = new();
     }
 }
