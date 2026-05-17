27f5948073d3752555c7bf4af5902d80f24d1232
Ethan Berry <ethanberry670@gmail.com>
2026-05-17 11:28:03 +0100

    Feat: Add new failing test

--- RecordShop/RecordShop.Tests/Unit/ServiceTests/ArtistServiceTests.cs
+++ RecordShop/RecordShop.Tests/Unit/ServiceTests/ArtistServiceTests.cs
@@ -1,6 +1,10 @@
-﻿using Moq;
+﻿using FluentAssertions;
+using Moq;
+using RecordShop.Api.Models.DataModels;
+using RecordShop.Api.Models.DTOs;
 using RecordShop.Api.Repositories;
 using RecordShop.Api.Services;
+using System.Threading.Tasks;
 
 namespace RecordShop.Tests.Unit.ServiceTests
 {
@@ -14,6 +18,21 @@ namespace RecordShop.Tests.Unit.ServiceTests
         {
             _artistRepositoryMock = new Mock<IArtistRepository>();
             _artistService = new ArtistService(_artistRepositoryMock.Object);
+        }
+
+        [Test]
+        public async Task GetAllArtistsAsync_ShouldReturnEmptyDTOList_WhenRepoMethodIsCalled()
+        {
+            var expectedList = new List<Artist>();
+
+            _artistRepositoryMock.Setup(a => a.GetAllArtistsAsync()).ReturnsAsync(expectedList);
+
+            var result = await _artistService.GetAllArtistsAsync();
+
+            result.Should().BeEmpty();
+            result.Should().BeEquivalentTo(expectedList);
+
+            _artistRepositoryMock.Verify(a => a.GetAllArtistsAsync(), Times.Once());
         }
     }
 }
