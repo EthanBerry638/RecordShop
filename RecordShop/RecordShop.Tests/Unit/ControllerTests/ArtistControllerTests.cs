1cf548eec5354d673c0a55cd830ee7befb1f0291
Ethan Berry <ethanberry670@gmail.com>
2026-05-17 11:55:40 +0100

    Feat: Add new failing test

--- RecordShop/RecordShop.Tests/Unit/ControllerTests/ArtistControllerTests.cs
+++ RecordShop/RecordShop.Tests/Unit/ControllerTests/ArtistControllerTests.cs
@@ -1,25 +1,44 @@
-﻿using Moq;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using RecordShop.Api.Controllers;
+using RecordShop.Api.Models.DTOs;
 using RecordShop.Api.Services;
 
 namespace RecordShop.Tests.Unit.ControllerTests
 {
     public class ArtistControllerTests
     {
-        private Mock<IArtistService> _mockService;
+        private Mock<IArtistService> _artistServiceMock;
         private ArtistController _artistController;
 
         [SetUp]
         public void Setup()
         {
-            _mockService = new Mock<IArtistService>();
-            _artistController = new ArtistController(_mockService.Object);
+            _artistServiceMock = new Mock<IArtistService>();
+            _artistController = new ArtistController(_artistServiceMock.Object);
         }
 
         [TearDown]
         public void TearDown()
         {
             _artistController.Dispose();
+        }
+
+        [Test]
+        public async Task GetAllArtistsAsync_ShouldReturnOkWithEmptyList_WhenServiceReturnsEmptyList()
+        {
+            var testList = new List<GetArtistResponse>();
+
+            _artistServiceMock.Setup(a => a.GetAllArtistsAsync()).ReturnsAsync(testList);
+
+            var result = await _artistController.GetAllArtistsAsync();
+
+            var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+            var value = okResult.Value as List<GetAlbumResponse>;
+
+            value.Should().NotBeNull();
+            value.Should().BeEmpty();
         }
     }
 }
