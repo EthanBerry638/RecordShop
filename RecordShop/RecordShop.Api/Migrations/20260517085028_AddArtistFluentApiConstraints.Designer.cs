d9fe3e23d8ca539de6061001c22d52f9f78fd4f9
Ethan Berry <ethanberry670@gmail.com>
2026-05-17 09:52:46 +0100

    Feat: Add migration for artist table

--- RecordShop/RecordShop.Api/Migrations/20260517085028_AddArtistFluentApiConstraints.Designer.cs
+++ RecordShop/RecordShop.Api/Migrations/20260517085028_AddArtistFluentApiConstraints.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecordShop.Api.Data;
 
@@ -11,9 +12,11 @@ using RecordShop.Api.Data;
 namespace RecordShop.Api.Migrations
 {
     [DbContext(typeof(RecordShopContext))]
-    partial class RecordShopContextModelSnapshot : ModelSnapshot
+    [Migration("20260517085028_AddArtistFluentApiConstraints")]
+    partial class AddArtistFluentApiConstraints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RecordShop/RecordShop.Api/Migrations/20260517085028_AddArtistFluentApiConstraints.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecordShop.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddArtistFluentApiConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
