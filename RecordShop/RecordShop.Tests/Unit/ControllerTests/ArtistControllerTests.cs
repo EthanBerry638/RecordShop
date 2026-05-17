a44faa1e950f83983f24d6cf51c5155b19c7ba0c
Ethan Berry <ethanberry670@gmail.com>
2026-05-17 11:59:57 +0100

    Feat: Add new test

--- RecordShop/RecordShop.Tests/Unit/ControllerTests/ArtistControllerTests.cs
+++ RecordShop/RecordShop.Tests/Unit/ControllerTests/ArtistControllerTests.cs
@@ -40,5 +40,29 @@ namespace RecordShop.Tests.Unit.ControllerTests
             value.Should().NotBeNull();
             value.Should().BeEmpty();
         }
+
+        [Test]
+        public async Task GetAllArtistsAsync_ShouldReturnOkWithSeededList_WhenServiceReturnsSeededList()
+        {
+            var testList = new List<GetArtistResponse>
+            {
+                new(1, "test", "test", 2),
+                new(2, "test", "test", 23),
+                new(3, "test", "test", 12),
+                new(4, "test", "test", 25),
+                new(5, "test", "test", 50)
+            };
+
+            _artistServiceMock.Setup(a => a.GetAllArtistsAsync()).ReturnsAsync(testList);
+
+            var result = await _artistController.GetAllArtistsAsync();
+
+            var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+            var value = okResult.Value as List<GetArtistResponse>;
+
+            value.Should().NotBeNull();
+            value.Should().NotBeEmpty();
+            value.Should().BeEquivalentTo(testList);
+        }
     }
 }
