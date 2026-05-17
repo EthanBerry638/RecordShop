2a765aa1a759552d26537838f97cb0a145260ec9
Ethan Berry <ethanberry670@gmail.com>
2026-05-17 12:59:34 +0100

    Feat: Setup artist integration testing

---
+++ RecordShop/RecordShop.Tests/Integration/ArtistIntegrationTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace RecordShop.Tests.Integration
+{
+    public class ArtistIntegrationTests
+    {
+        private WebApplicationFactory<Program> _factory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _factory = new WebApplicationFactory<Program>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _factory.Dispose();
+        }
+
+
+    }
+}
