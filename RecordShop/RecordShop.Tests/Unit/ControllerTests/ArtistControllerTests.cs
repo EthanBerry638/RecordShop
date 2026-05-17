8591db6e62f493a377ae5563f24fd4d0953078d2
Ethan Berry <ethanberry670@gmail.com>
2026-05-17 11:53:01 +0100

    Feat: Add setup for artist controller testing

---
+++ RecordShop/RecordShop.Tests/Unit/ControllerTests/ArtistControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using RecordShop.Api.Controllers;
+using RecordShop.Api.Services;
+
+namespace RecordShop.Tests.Unit.ControllerTests
+{
+    public class ArtistControllerTests
+    {
+        private Mock<IArtistService> _mockService;
+        private ArtistController _artistController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockService = new Mock<IArtistService>();
+            _artistController = new ArtistController(_mockService.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _artistController.Dispose();
+        }
+    }
+}
