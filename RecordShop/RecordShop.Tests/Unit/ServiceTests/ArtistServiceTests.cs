58566d05fa4fe3a3bb3a8e53b674eed471a70b44
Ethan Berry <ethanberry670@gmail.com>
2026-05-17 10:39:47 +0100

    Feat: Add setup for artist service tests

---
+++ RecordShop/RecordShop.Tests/Unit/ServiceTests/ArtistServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using RecordShop.Api.Repositories;
+using RecordShop.Api.Services;
+
+namespace RecordShop.Tests.Unit.ServiceTests
+{
+    public class ArtistServiceTests
+    {
+        private Mock<IArtistRepository> _artistRepositoryMock;
+        private ArtistService _artistService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _artistRepositoryMock = new Mock<IArtistRepository>();
+            _artistService = new ArtistService(_artistRepositoryMock.Object);
+        }
+    }
+}
